99ec4748c426e4bfa20bf26b9d1e9b1403348056
nightangelblade <sean.syc.wright@gmail.com>
2016-12-17 01:11:50 -0800

    Edit dice.rb

--- dice.rb
+++ dice.rb
@@ -14,12 +14,4 @@ class Roller
 		database.execute("INSERT INTO rolls (battle, craft, loot, enemy, name, link, post_time) VALUES (?, ?, ?, ?, ?, ?, ?)", [@battle, @craft, @loot, @enemy, @name, @url, @timestamp])
 	end
 
-	def search_rolls_name(database, character_name)
-		p database.execute("SELECT * FROM rolls WHERE name=?", [character_name])
-	end
-
-	def search_rolls_id(database, roll_id)
-		p database.execute("SELECT * FROM rolls WHERE id=?", [roll_id])
-	end
-
 end
